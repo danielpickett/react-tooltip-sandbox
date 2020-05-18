40e6341088c29d28819b31415e8ca35f6cd00199
danielpickett <daniel@danielpickett.me>
2020-05-18 18:02:54 -0400

    😊

--- src/App.tsx
+++ src/App.tsx
@@ -4,87 +4,85 @@ import { Tooltip } from './components/Tooltip/Tooltip'
 import { Portal } from './components/Portal/Portal'
 
 function App() {
-  const [tooltipActive, setTooltipActive] = useState(false)
+  const [tooltipIsActive, setTooltipIsActive] = useState(false)
   return (
     <div className="App" style={{ overflow: 'auto' }}>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
-        praesentium quod minus similique atque totam laboriosam nemo rem
-        excepturi consectetur? Laudantium impedit delectus tempore officia
-        consequatur maxime animi molestiae odio iste libero, a fugiat
-        perferendis minus necessitatibus vero, temporibus nulla quibusdam
-        voluptate tempora nostrum recusandae, cum repudiandae dolor error. Iure
-        quis blanditiis iste eaque maxime deserunt harum cupiditate ipsum
-        laborum quasi, odit eligendi reprehenderit magni debitis sequi sed
-        dolorum? Sapiente, hic! Consequuntur ad eos exercitationem distinctio,
-        rem laborum aliquam animi aspernatur, ratione incidunt dolor
-        reprehenderit fugit expedita. Voluptatum, quibusdam quis quisquam
-        aliquid in atque deleniti, hic, ab optio laborum tempora?
-      </p>
-      <button
-        className="App__button"
-        onClick={(e) => {
-          setTooltipActive(!tooltipActive)
-        }}
-      >
-        Show tooltip
-      </button>
-      {tooltipActive && (
-        <Portal>
-          <Tooltip>
-            <button
-              className="App__button"
-              onClick={(e) => {
-                setTooltipActive(!tooltipActive)
-              }}
-            >
-              Show tooltip
-            </button>
-          </Tooltip>
-        </Portal>
-      )}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
-        praesentium quod minus similique atque totam laboriosam nemo rem
-        excepturi consectetur? Laudantium impedit delectus tempore officia
-        consequatur maxime animi molestiae odio iste libero, a fugiat
-        perferendis minus necessitatibus vero, temporibus nulla quibusdam
-        voluptate tempora nostrum recusandae, cum repudiandae dolor error. Iure
-        quis blanditiis iste eaque maxime deserunt harum cupiditate ipsum
-        laborum quasi, odit eligendi reprehenderit magni debitis sequi sed
-        dolorum? Sapiente, hic! Consequuntur ad eos exercitationem distinctio,
-        rem laborum aliquam animi aspernatur, ratione incidunt dolor
-        reprehenderit fugit expedita. Voluptatum, quibusdam quis quisquam
-        aliquid in atque deleniti, hic, ab optio laborum tempora?
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
-        praesentium quod minus similique atque totam laboriosam nemo rem
-        excepturi consectetur? Laudantium impedit delectus tempore officia
-        consequatur maxime animi molestiae odio iste libero, a fugiat
-        perferendis minus necessitatibus vero, temporibus nulla quibusdam
-        voluptate tempora nostrum recusandae, cum repudiandae dolor error. Iure
-        quis blanditiis iste eaque maxime deserunt harum cupiditate ipsum
-        laborum quasi, odit eligendi reprehenderit magni debitis sequi sed
-        dolorum? Sapiente, hic! Consequuntur ad eos exercitationem distinctio,
-        rem laborum aliquam animi aspernatur, ratione incidunt dolor
-        reprehenderit fugit expedita. Voluptatum, quibusdam quis quisquam
-        aliquid in atque deleniti, hic, ab optio laborum tempora?
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
-        praesentium quod minus similique atque totam laboriosam nemo rem
-        excepturi consectetur? Laudantium impedit delectus tempore officia
-        consequatur maxime animi molestiae odio iste libero, a fugiat
-        perferendis minus necessitatibus vero, temporibus nulla quibusdam
-        voluptate tempora nostrum recusandae, cum repudiandae dolor error. Iure
-        quis blanditiis iste eaque maxime deserunt harum cupiditate ipsum
-        laborum quasi, odit eligendi reprehenderit magni debitis sequi sed
-        dolorum? Sapiente, hic! Consequuntur ad eos exercitationem distinctio,
-        rem laborum aliquam animi aspernatur, ratione incidunt dolor
-        reprehenderit fugit expedita. Voluptatum, quibusdam quis quisquam
-        aliquid in atque deleniti, hic, ab optio laborum tempora?
-      </p>
+      <div className="App__wrapper-1">
+        <div className="App__wrapper-2">
+          <div className="App__wrapper-3">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+              praesentium quod minus similique atque totam laboriosam nemo rem
+              excepturi consectetur? Laudantium impedit delectus tempore officia
+              consequatur maxime animi molestiae odio iste libero, a fugiat
+              perferendis minus necessitatibus vero, temporibus nulla quibusdam
+              voluptate tempora nostrum recusandae, cum repudiandae dolor error.
+              Iure quis blanditiis iste eaque maxime deserunt harum cupiditate
+              ipsum laborum quasi, odit eligendi reprehenderit magni debitis
+              sequi sed dolorum? Sapiente, hic! Consequuntur ad eos
+              exercitationem distinctio, rem laborum aliquam animi aspernatur,
+              ratione incidunt dolor reprehenderit fugit expedita. Voluptatum,
+              quibusdam quis quisquam aliquid in atque deleniti, hic, ab optio
+              laborum tempora?
+            </p>
+            <Tooltip isActive={tooltipIsActive}>
+              <button
+                className="App__button"
+                onClick={(e) => {
+                  setTooltipIsActive(!tooltipIsActive)
+                }}
+              >
+                Show tooltip
+              </button>
+            </Tooltip>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+              praesentium quod minus similique atque totam laboriosam nemo rem
+              excepturi consectetur? Laudantium impedit delectus tempore officia
+              consequatur maxime animi molestiae odio iste libero, a fugiat
+              perferendis minus necessitatibus vero, temporibus nulla quibusdam
+              voluptate tempora nostrum recusandae, cum repudiandae dolor error.
+              Iure quis blanditiis iste eaque maxime deserunt harum cupiditate
+              ipsum laborum quasi, odit eligendi reprehenderit magni debitis
+              sequi sed dolorum? Sapiente, hic! Consequuntur ad eos
+              exercitationem distinctio, rem laborum aliquam animi aspernatur,
+              ratione incidunt dolor reprehenderit fugit expedita. Voluptatum,
+              quibusdam quis quisquam aliquid in atque deleniti, hic, ab optio
+              laborum tempora?
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+              praesentium quod minus similique atque totam laboriosam nemo rem
+              excepturi consectetur? Laudantium impedit delectus tempore officia
+              consequatur maxime animi molestiae odio iste libero, a fugiat
+              perferendis minus necessitatibus vero, temporibus nulla quibusdam
+              voluptate tempora nostrum recusandae, cum repudiandae dolor error.
+              Iure quis blanditiis iste eaque maxime deserunt harum cupiditate
+              ipsum laborum quasi, odit eligendi reprehenderit magni debitis
+              sequi sed dolorum? Sapiente, hic! Consequuntur ad eos
+              exercitationem distinctio, rem laborum aliquam animi aspernatur,
+              ratione incidunt dolor reprehenderit fugit expedita. Voluptatum,
+              quibusdam quis quisquam aliquid in atque deleniti, hic, ab optio
+              laborum tempora?
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+              praesentium quod minus similique atque totam laboriosam nemo rem
+              excepturi consectetur? Laudantium impedit delectus tempore officia
+              consequatur maxime animi molestiae odio iste libero, a fugiat
+              perferendis minus necessitatibus vero, temporibus nulla quibusdam
+              voluptate tempora nostrum recusandae, cum repudiandae dolor error.
+              Iure quis blanditiis iste eaque maxime deserunt harum cupiditate
+              ipsum laborum quasi, odit eligendi reprehenderit magni debitis
+              sequi sed dolorum? Sapiente, hic! Consequuntur ad eos
+              exercitationem distinctio, rem laborum aliquam animi aspernatur,
+              ratione incidunt dolor reprehenderit fugit expedita. Voluptatum,
+              quibusdam quis quisquam aliquid in atque deleniti, hic, ab optio
+              laborum tempora?
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,17 +1,51 @@
-import React, { useEffect, ReactNode } from 'react'
+import React, { ReactElement, useCallback, useState, cloneElement } from 'react'
 import './Tooltip.scss'
+import { Portal } from '../Portal/Portal'
+import * as ReactIs from 'react-is'
 
 export const Tooltip = ({
-  children
+  children,
+  isActive,
 }: {
-  children: ReactNode
+  children: ReactElement
+  isActive: boolean
 }) => {
-  
-  
+  if (ReactIs.isFragment(children))
+    throw new Error(
+      'Tooltip component must have only one child that is not a React.Fragment'
+    )
+
+  const [coords, setCoords] = useState<{ left: number; top: number } | null>()
+
+  const callbackRef = useCallback((node) => {
+    if (node !== null) {
+      const rect = node.getBoundingClientRect()
+      console.log(ReactIs.isFragment(node))
+      console.log(rect)
+      setCoords({
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY,
+      })
+    }
+  }, [])
 
   return (
-    <div className="Tooltip">
-      Hello from the Tooltip component
-    </div>
+    <>
+      <div className="Tooltip__container">
+        {cloneElement(children, { ref: callbackRef })}
+        {/* {children} */}
+      </div>
+
+      {isActive && (
+        <Portal>
+          <div
+            className="Tooltip"
+            style={{ left: coords?.left, top: coords?.top }}
+          >
+            Hello from the Tooltip component
+          </div>
+        </Portal>
+      )}
+    </>
   )
 }
